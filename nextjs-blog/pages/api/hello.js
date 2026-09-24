d90359978a12ab8fb98df18e65bae89b06d85790
Travis DeCarlo <travis.business83@gmail.com>
2026-09-24 01:44:30 -0700

    commit v2

---
+++ nextjs-blog/pages/api/hello.js
@@ -0,0 +1,7 @@
+
+// creating the below function inside pages/api dir will provide an api path
+//req is an instance of http.IncomingMessage plus middleware
+// res is an instance of http.ServerResponse plus helper functions
+export default function handler(req, res) {
+    res.status(200).json({ text: 'hello'});
+}
